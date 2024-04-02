436f9183a33a6c4fe63c5042e8813afd0ac05ce1
Siddharth0101 <147489528+Siddharth0101@users.noreply.github.com>
2024-04-02 12:44:30 +0530

    Compose

--- src/pages/Compose/Compose.js
+++ src/pages/Compose/Compose.js
@@ -1,4 +1,58 @@
+import React, { useState } from "react";
+import ReactQuill from "react-quill";
+import "react-quill/dist/quill.snow.css";
+
 const Compose = () => {
-  return <div>Compose</div>;
+  const [content, setContent] = useState("");
+
+  const modules = {
+    toolbar: [
+      [{ header: "1" }, { header: "2" }, { font: [] }],
+      [{ size: [] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" },
+      ],
+      ["link", "image", "video"],
+      ["clean"],
+    ],
+  };
+
+  const formats = [
+    "header",
+    "font",
+    "size",
+    "bold",
+    "italic",
+    "underline",
+    "strike",
+    "blockquote",
+    "list",
+    "bullet",
+    "indent",
+    "link",
+    "image",
+    "video",
+  ];
+
+  return (
+    <div className="compose-container">
+      <input type="text" placeholder="To" />
+      <input type="text" placeholder="Subject" />
+      <ReactQuill
+        theme="snow"
+        value={content}
+        onChange={setContent}
+        modules={modules}
+        formats={formats}
+        placeholder="Compose your email..."
+      />
+      <button>Send</button>
+    </div>
+  );
 };
+
 export default Compose;
